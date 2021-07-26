185453b8ca6257c719379fb5e7b5aa781db0d316
ralucaneagu <ralucaa.an99@gmail.com>
2021-07-26 18:34:16 +0300

    HM-10-post component

---
+++ hermes-main-website/src/app/components/post/post.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.sass']
+})
+export class PostComponent implements OnInit {
+  @Input()
+    data?:string
+
+  @Input()
+    imagine?:string
+
+    @Input()
+    text?:string
+
+  constructor() {
+    //
+   }
+
+  ngOnInit(): void {
+    //
+  }
+
+}
