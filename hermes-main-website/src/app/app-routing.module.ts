a66cbf95590bfda8dc77a3c802cc4b3b63ac79be
Zotta <victor.zotta@gmail.com>
2021-07-30 11:27:39 +0300

    HM-12/routers-without-posts

--- hermes-main-website/src/app/app-routing.module.ts
+++ hermes-main-website/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './views/projects/projects/projects.component';
+import { CdComponent } from './views/cd/cd/cd.component';
+import { ContactComponent } from './views/contact/contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+      path:"projects",
+    component : ProjectsComponent
+  },
+  {
+    path:"cd",
+  component : CdComponent
+},
+{
+  path:"contact",
+component : ContactComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
