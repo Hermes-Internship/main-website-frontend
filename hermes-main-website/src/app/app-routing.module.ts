b76b15699b92eb4bdfb4c1e6f01a0f59205ffd60
Zotta <victor.zotta@gmail.com>
2021-08-04 10:01:16 +0300

    Hm-12/post router link 50%

--- hermes-main-website/src/app/app-routing.module.ts
+++ hermes-main-website/src/app/app-routing.module.ts
@@ -3,10 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './views/projects/projects/projects.component';
 import { CdComponent } from './views/cd/cd/cd.component';
 import { ContactComponent } from './views/contact/contact/contact.component';
+import { MainComponent } from './views/main/main/main.component';
 
 
 const routes: Routes = [
-   
+   {
+     path:"",
+     redirectTo : "main",
+     pathMatch : "full"
+   } ,
+   {
+    path:"main",
+    component : MainComponent
+  },
   {
     path:"projects",
     component : ProjectsComponent
@@ -16,9 +25,14 @@ const routes: Routes = [
   component : CdComponent
   },
   {
+    path:"posts",
+    component : MainComponent
+    },
+  {
   path:"contact",
 component : ContactComponent
   }
+
 ];
 
 @NgModule({
